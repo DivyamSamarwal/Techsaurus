30437a66710e397592873a38643413adee73c5a2
Divyam Samarwal <divyamsamarwal@gmail.com>
2022-03-07 16:11:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,7 +95,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/doc2')}>
               Get Started
             </Link>
           </div>
